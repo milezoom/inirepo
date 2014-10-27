26a699183865c28499153cc248c995af4edec27f
muslim <mu51im.x28@gmail.com>
2014-10-27 22:02:20 +0700

    tambah kodingan yang masih gak jelas maksudnya

--- iniprogram.c
+++ iniprogram.c
@@ -3,12 +3,8 @@
 
 int main()
 {
-<<<<<<< HEAD
   puts("Hello World!\n");
   puts("initambahlinelagi\n");
   puts("initambahlineterakhir");
-=======
-  puts("Hello World!");
->>>>>>> master
   return 0;
 }
